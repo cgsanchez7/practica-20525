e82d605a0305f35ebb91d8ce8ea19258b312a589
bfguerrero1 <bfguerrero1@espe.edu.ec>
2025-04-25 08:06:49 -0500

    suma

--- test.cpp
+++ test.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
 using namespace std;
-int main(){
+
+
+#include <stdio.h>
+
+
+float sumar(float a, float b);
+
+
+int main() {
+
+    }
+
+
+
+float sumar(float a, float b) {
+    return a + b;
 }
+
