9b4dce4156e78abeddbf1202ccfcddee6828df84
cgsanchez7 <cgsanchez7@espe.edu.ec>
2025-04-25 08:36:56 -0500

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "CALCULADORA" << endl;
+
+#include <stdio.h>
+
+
+float resta(float a, float b);
+float suma(float a, float b);
+float multiplicacion(float a, float b);
+float division(float a, float b);
+int main() {
+
+    }
+
+
+
+float suma(float a, float b) {
+    return a + b;
+}
+float resta(float a, float b) {
+    return a - b;
+}
+float multiplicacion(float a, float b) {
+    return a * b;
+
+}
+float division(float a, float b) {
+    return a/b;
 }
